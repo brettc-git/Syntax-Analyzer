c1ff58f6c9cad48a976967e3443f8808a50d4a05
Brett Chiu <72076153+brettc-git@users.noreply.github.com>
2025-04-04 10:07:42 -0700

    Update syntax.py

--- syntax.py
+++ syntax.py
@@ -1,9 +1,22 @@
+
 def Rat25S():
   pass 
 
 def optFunctionDefinitions():
   pass
 
+def functionDefinitions():
+  pass
+  
+def function():
+  pass
+
+def optParameterList():
+  pass 
+
+def parameterList(): 
+  pass 
+  
 def parameter():
   pass 
 
@@ -37,5 +50,11 @@ def compound():
 def assign():
   pass
 
+def _if():
+  pass
+
+def ifPrime():
+  pass
+  
 def empty():
   pass 
